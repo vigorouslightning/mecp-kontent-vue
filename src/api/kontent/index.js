c92e14a2a9e2f9b010efa031c32f8d7bf8a2c859
Kyle Plunkett <kplunkett@ce.org>
2020-03-30 21:28:57 +0100

    Starting to fillout Kontent api JS

--- src/api/kontent/index.js
+++ src/api/kontent/index.js
@@ -1,1 +1,34 @@
-// placeholder for kontent api js
+const KontentDelivery = require('@kentico/kontent-delivery');
+let Page = require('@/models/page');
+let Homepage = require('@/models/homepage');
+
+const deliveryClient = new KontentDelivery.DeliveryClient({
+    projectId: '2f957d12-d69d-00a1-bdf9-fb08ef04dfa5',
+    typeResolvers: [
+        new KontentDelivery.TypeResolver('page', (rawData) => new Page.Page()),
+        new KontentDelivery.TypeResolver('homepage', (rawData) => new Homepage.Homepage())
+    ]
+});
+
+export default {
+/** Getting items from Kentico Kontent as Observable */
+    async getHomePage() {
+        return await deliveryClient.items()
+        .type('homepage')
+        .toPromise()
+        .then(response => {
+            //console.log('inside: ' + JSON.stringify(response));
+            return response.items[0];
+        });
+    },
+
+    async bySlug(slug) {
+        return await deliveryClient.items()
+        .type('page')
+        .equalsFilter('elements.slug', slug)
+        .toPromise()
+        .then(response => {
+            return response.items[0];
+        });
+    }
+}
