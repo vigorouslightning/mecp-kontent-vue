aa88c91d22c2a6481d015ac3c71e0b2c0bd18fe9
Kyle Plunkett <kplunkett@ce.org>
2020-04-01 06:57:05 +0100

    Hardcoded Main MECP logo for now

--- src/components/Header/Logo.vue
+++ src/components/Header/Logo.vue
@@ -31,10 +31,10 @@ export default {
   mounted: function() {},
     methods: {
     async byId(id) {
-      this.logo = 'kyletest.jpg'; //(await api.contentful.getAsset(id)).fields.file.url;
+      this.logo = 'https://assets-us-01.kc-usercontent.com/2f957d12-d69d-00a1-bdf9-fb08ef04dfa5/4218a351-2edf-48ea-9ed5-a9e8e192c742/MECPLogo.png'; //(await api.contentful.getAsset(id)).fields.file.url;
     },
     init() {
-      let id = process.env.VUE_APP_HOMEPAGE_LOGO;
+      let id = process.env.VUE_APP_HOMEPAGE_LOGO; //this seems like more work than just updating the logo directly.
       this.byId(id);
     }
   }
